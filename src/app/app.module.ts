7721630d022fe79e4cdc668071db693ec578ac4f
Nikoloz Razmadze <raloz1@gmail.com>
2019-12-19 18:33:46 +0400

    Migration part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './components/app/app.component';
 import { PickerComponent } from './components/picker/picker.component';
 import { BackgroundComponent } from './components/background/background.component';
-import {FullscreenService} from "./services/fullscreen.service";
-import {ColorService} from "./services/color.service";
+import { FullscreenService } from './services/fullscreen.service';
+import { ColorService } from './services/color.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +17,6 @@ import {ColorService} from "./services/color.service";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
   ],
   providers: [
     FullscreenService,
